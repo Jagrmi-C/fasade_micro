ae10add77e5417ded85998964db4ab72b28237cb
Andrei Ramanchyk <andrey.romanchik@solbeg.com>
2020-07-29 20:57:06 +0300

    Add endpoints

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import uvicorn
 
+from typing import Optional
+
 from fastapi import FastAPI
+
+from models import ModelName
+
 
 app = FastAPI()
 
@@ -8,6 +13,39 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/model/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_item(
+    user_id: int,
+    item_id: str,
+    q: Optional[str] = None, short: bool = False,
+):
+    # import pdb; pdb.set_trace()
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
 
 
 if __name__ == "__main__":
